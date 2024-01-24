fd3e6f7eb18bfdebf1fb1ecf3eed25db392eeaef
Ryan Rivest <ryan.rivest@gmail.com>
2024-01-23 21:02:31 -0800

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+
+<script>
+    import Card from './Card.svelte';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        const bdsElement = document.createElement('bds');
+        document.head.appendChild(bdsElement);
+    });
+</script>
+
+<Card />
---
+++ src/routes/Card.svelte
@@ -0,0 +1,14 @@
+<section class="card">
+</section>
+
+<style>
+	.card {
+	    margin: auto;
+        height: 25rem;
+        width: 100%;
+        max-width: 50rem;
+        background-color: var(--color-bg-card);
+        border-radius: 2rem;
+        box-shadow: var(--shadow-card);
+	}
+</style>
